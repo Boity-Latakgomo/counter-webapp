8dc402a419fbcf22dc3a6afaf9df7826e5920a4b
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-10-21 10:47:18 +0200

    raising and handling handleDelete event handler

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,18 +4,20 @@ import Counter from "./counter";
 class Counters extends Component {
     state = { 
         counters: [
-            {id: 1, value:0},
-            {id: 2, value:0},
-            {id: 3, value:0},
-            {id: 4, value:0},
+            {id: 1, value:1},
+            {id: 2, value:2},
+            {id: 3, value:3},
+            {id: 4, value:4}
         ]
      } ;
+     handleDelete = () => {
+        console.log('Event Handler Called');
+     };
     render() { 
         return (
             <div>
             {this.state.counters.map(counter => (
-                <Counter key={counter.id } value={counter.value}>
-           <h4>Counter #{counter.id}</h4>
+                <Counter key={counter.id } onDelete={this.handleDelete} value={counter.value} id={counter.id}>
            </Counter>
         ))}
             </div>
