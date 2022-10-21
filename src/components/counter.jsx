95d2091879f884440e049c6456fc12bea199a008
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-10-21 10:39:34 +0200

    raising and handling handleDelete event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,13 +14,16 @@ class Counter extends Component {
         let classes= "badge m-2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary";
 
-      console.log(this.props);
+      
         return (
             <div> 
-            {this.props.children}
+            <h4>{this.props.id}</h4>
             <span className={classes}>{this.formatCount()}</span>
             <button onClick={() => this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
+
+
             );
     }
     formatCount(){
