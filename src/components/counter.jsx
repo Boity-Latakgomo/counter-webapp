a145f8dd5254cc077aec773752c67620769f93e0
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-10-23 13:29:26 +0200

    Reset functionality is working fine

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,32 +2,29 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class Counter extends Component {
-    state={
-        count: this.props.value
-    };
-    handleIncrement = e => {
-       console.log(e);
-       this.setState({count: this.state.count + 1 });
-    };
+    render(){
+    return (
+        <div> 
+        <h4>{this.props.id}</h4>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+        </div>
 
-    render() { 
+
+        );
+    }
+
+    getBadgeClasses() {
         let classes= "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-
-        return (
-            <div> 
-            <h4>{this.props.id}</h4>
-            <span className={classes}>{this.formatCount()}</span>
-            <button onClick={() => this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Increment</button>
-            <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
-            </div>
-
-
-            );
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
+        return classes;
+    
+        
     }
     formatCount(){
-        const {count} = this.state;
-        return count === 0 ? "zero" : count;
+        const {value} = this.props.counter;
+        return value === 0 ? "zero" : value;
     }
 }
  
