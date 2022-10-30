85ac6a6f7bc55c3b89477b47ab8b1b977d2a9418
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-10-30 21:45:56 +0200

    navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import navBar from './components/navBar';
 import './App.css';
 import React, { Component } from 'react';
 import Counters from './components/counters';
 
 
-function App() {
-  return <Counters />
-}
+class App extends Component {
+  render(){
+  return (
+    <React.Fragment>
+   <navBar /> 
+   <main className="container">
+   <Counters />
+   </main>
+   </React.Fragment>
 
+  );
+  }
+  
+}
 export default App;
